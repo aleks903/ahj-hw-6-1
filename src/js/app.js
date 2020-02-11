b5ba145347f6724b92f5f8d6dd6055734c69edf3
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2020-02-11 23:22:45 +0300

    ahj-hw-6-1

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,9 @@
-// import Popovers from './Popovers.js';
+import WorkDislay from './WorkDisplay.js';
+import initData from './initData.js';
+import Storage from './Storage.js';
 
-// const elementButton = document.getElementById('button');
-// const addPopup = new Popovers(document.body, 'Popover title', 'And here`s some amazing content. It`s very engaging. Right?');
-// addPopup.bindToDOM();
-
-// elementButton.addEventListener('click', () => {
-//   addPopup.positionPopup(elementButton);
-// });
+const storage = new Storage();
+const workDisplay = new WorkDislay();
 
 let draggedEl = null;
 let ghostEl = null;
@@ -16,91 +13,146 @@ let elTop;
 let elLeft;
 const elTasks = document.querySelector('#tasks');
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('load');
+  const storageData = JSON.parse(storage.load());
+  if(storageData !== null) {
+    workDisplay.initTasks(storageData);
+  } else {
+    workDisplay.initTasks(initData());
+  }
+});
+
 // mousedown
 elTasks.addEventListener('mousedown', (event) => {
-  event.preventDefault();
-  if (!event.target.classList.contains('item-task')) {
-    if(event.target.classList.contains('add-task')) {
-      event.target.parentNode.querySelector('.input-task').classList.remove('hidden');
-      event.target.classList.add('hidden');
-    }
-    return;
+  // event.preventDefault();
+  if(event.target.classList.contains('add-task')) {
+    event.target.parentNode.querySelector('.input-task').classList.remove('hidden');
+    event.target.classList.add('hidden');
+
+  } else if (event.target.classList.contains('b-cancel-task')) {
+    event.target.closest('.col-tasks').querySelector('.add-task').classList.remove('hidden');
+    event.target.parentNode.classList.add('hidden');
+
+  } else if (event.target.classList.contains('b-add-task')) {
+    const elAddTask = event.target.closest('.col-tasks').querySelector('.item-tasks');
+    const elInput = event.target.closest('.input-task').querySelector('#text-task');
+    workDisplay.addTask(elAddTask, elInput.value);
+    elInput.value = '';
+    event.target.closest('.col-tasks').querySelector('.add-task').classList.remove('hidden');
+    event.target.parentNode.classList.add('hidden');
+    toObjectTasks();
+
+  } else if (event.target.classList.contains('del-task')) {
+    console.log('del-task');
+    const itemDel = event.target.parentNode;
+    itemDel.parentNode.removeChild(itemDel);
+    toObjectTasks();
+    
+  } else if (event.target.classList.contains('item-task')) {
+    console.log(event.target.parentNode)
+    event.preventDefault();
+    event.target.querySelector('.del-task').classList.add('hidden');
+    const { top, left } = event.target.getBoundingClientRect();
+    draggedEl = event.target;
+    elWidth = draggedEl.offsetWidth;
+    elHeight = draggedEl.offsetHeight;
+    elLeft = event.pageX - left;
+    elTop = event.pageY - top;
+
+    ghostEl = event.target.cloneNode(true);
+    ghostEl.innerHTML = '';
+    ghostEl.style.backgroundColor = 'grey';
+    ghostEl.style.width = `${elWidth}px`;
+    ghostEl.style.height = `${elHeight}px`;
+
+    draggedEl.classList.add('dragged');
+    event.target.parentNode.insertBefore(ghostEl, event.target.nextElementSibling);
+
+    draggedEl.style.left = `${event.pageX - elLeft}px`;
+    draggedEl.style.top = `${event.pageY - elTop}px`;
+    draggedEl.style.width = `${elWidth}px`;
+    draggedEl.style.height = `${elHeight}px`;
   }
-  const { top, left } = event.target.getBoundingClientRect();
-  draggedEl = event.target;
-  elWidth = draggedEl.offsetWidth;
-  elHeight = draggedEl.offsetHeight;
-  elLeft = event.pageX - left;
-  elTop = event.pageY - top;
-
-  ghostEl = event.target.cloneNode(true);
-  ghostEl.innerHTML = '';
-  ghostEl.style.backgroundColor = 'grey';
-  ghostEl.style.width = `${elWidth}px`;
-  ghostEl.style.height = `${elHeight}px`;
-
-  draggedEl.classList.add('dragged');
-  event.target.parentNode.insertBefore(ghostEl, event.target.nextElementSibling);
-
-  draggedEl.style.left = `${event.pageX - elLeft}px`;
-  draggedEl.style.top = `${event.pageY - elTop}px`;
-  draggedEl.style.width = `${elWidth}px`;
-  draggedEl.style.height = `${elHeight}px`;
-
 });
 
+// mouseleave
 elTasks.addEventListener('mouseleave', (event) => {
-  if (!draggedEl) {
-    return;
+  if (draggedEl) {
+    event.preventDefault();
+    console.log('mouseLeave');
+    ghostEl.parentNode.removeChild(ghostEl);
+    draggedEl.classList.remove('dragged');
+    draggedEl.style =''
+    ghostEl = null;
+    draggedEl = null;
+    //return;
   }
-console.log('mouseLeave');
-  ghostEl.parentNode.removeChild(ghostEl);
-  draggedEl.classList.remove('dragged');
-  draggedEl.style =''
-  ghostEl = null;
-  draggedEl = null;
 });
 
+// mousemove
 elTasks.addEventListener('mousemove', (event) => {
-  event.preventDefault();
-  if (!draggedEl) {
-    return;
+  if (draggedEl) {
+    event.preventDefault();
+    elDragDrop(event, ghostEl);
+    draggedEl.style.left = `${event.pageX - elLeft}px`;
+    draggedEl.style.top = `${event.pageY - elTop}px`;
   }
-  
-  const closest = document.elementFromPoint(event.clientX, event.clientY);
-  const { top } = closest.getBoundingClientRect();
-
-  if (!closest.classList.contains('item-tasks')) {
-    if (event.pageY > window.scrollY + top + closest.offsetHeight / 2) {
-      closest.closest('.item-tasks').insertBefore(ghostEl, closest.nextElementSibling);
-    } else {
-      closest.closest('.item-tasks').insertBefore(ghostEl, closest);
-    }
-  }
-
-  draggedEl.style.left = `${event.pageX - elLeft}px`;
-  draggedEl.style.top = `${event.pageY - elTop}px`;
 });
 
-
+// mouseup
 elTasks.addEventListener('mouseup', (event) => {
-  if (!draggedEl) {
-    return;
+  if (draggedEl) {
+    elDragDrop(event, draggedEl);
+
+    ghostEl.parentNode.removeChild(ghostEl);
+    draggedEl.classList.remove('dragged');
+    draggedEl.style =''
+    ghostEl = null;
+    draggedEl = null;
+    
+    toObjectTasks();
   }
+});
+
+function elDragDrop(event, element) {
   const closest = document.elementFromPoint(event.clientX, event.clientY);
   const { top } = closest.getBoundingClientRect();
-  
-  if (!closest.classList.contains('item-tasks')) {
+
+  if (closest.classList.contains('item-task')) {
     if (event.pageY > window.scrollY + top + closest.offsetHeight / 2) {
-      closest.closest('.item-tasks').insertBefore(draggedEl, closest.nextElementSibling);
+      closest.closest('.item-tasks').insertBefore(element, closest.nextElementSibling);
     } else {
-      closest.closest('.item-tasks').insertBefore(draggedEl, closest);
+      closest.closest('.item-tasks').insertBefore(element, closest);
     }
+  } else if (closest.classList.contains('item-tasks') && !closest.querySelector('.item-task')) {
+    closest.append(element);
+  }
+}
+
+
+
+function toObjectTasks() {
+  const toDoTasks = document.querySelectorAll('#todo .item-tasks .item-task');
+  const inProgressTasks = document.querySelectorAll('#in-progress .item-tasks .item-task');
+  const doneTasks = document.querySelectorAll('#done .item-tasks .item-task');
+
+  const objTasks = {
+    todo: [],
+    inProgress: [],
+    done: [],
+  };
+  
+  for (let item of toDoTasks) {
+    objTasks.todo.push(item.textContent.replace(' ✖', ''));
   }
 
-  ghostEl.parentNode.removeChild(ghostEl);
-  draggedEl.classList.remove('dragged');
-  draggedEl.style =''
-  ghostEl = null;
-  draggedEl = null;
-});
+  for (let item of inProgressTasks) {
+    objTasks.inProgress.push(item.textContent.replace(' ✖', ''));
+  }
+
+  for (let item of doneTasks) {
+    objTasks.done.push(item.textContent.replace(' ✖', ''));
+  }
+  storage.save(objTasks);
+}
---
+++ src/js/initData.js
@@ -0,0 +1,7 @@
+export default function initData() {
+  return {
+    todo: ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6'],
+    inProgress: ['Task 1', 'Task 2', 'Task 3', 'Task 4'],
+    done: ['Task 1', 'Task 2'],
+  };
+}
